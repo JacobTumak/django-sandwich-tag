2a7ab6cef3c3b44f09c69b334c68babc995184a6
JacobTumak <jacobtumak@gmail.com>
2025-08-23 16:03:51 -0700

    reduced code duplication

--- sandwich_tag/helpers.py
+++ sandwich_tag/helpers.py
@@ -1,41 +1,21 @@
 from functools import wraps
 from inspect import unwrap, getfullargspec
-from typing import TypeAlias
 
-from django import template
-from django.template import Template, Context
+from django.template import Context
 from django.template.loader import get_template
-from django.template.base import FilterExpression, Parser, Token
 from django.template.library import Library, parse_bits, TagHelperNode
 
 register = Library()
 
-TemplateSpec: TypeAlias = FilterExpression
-
-def resolve_template_spec(template_spec: TemplateSpec, context: Context) -> Template:
-    template_spec = template_spec.resolve(context)
-    match template_spec:
-        case str():
-            if '{{' in template_spec or '{%' in template_spec:
-                return Template(template_spec, engine=context.template.engine)
-            return context.template.engine.get_template(template_spec).template
-        case Template():
-            return template_spec
-        case _:
-            raise template.TemplateSyntaxError(
-                f"template param must be a string or a Template object, got {template_spec} instead."
-            )
-
-
-def make_sandwich_tag(
-        func=None,
-        template: str = None,
+def get_compile_func(
+        func,
+        template: str,
         name: str = None,
         takes_context=False,
         child_var_name='sandwich_fixings'
 ):
-    assert template is not None, "Custom sandwich tags require a template"
     template = get_template(template)
+    function_name = name or func.__name__
     (
         params,
         varargs,
@@ -45,10 +25,11 @@ def make_sandwich_tag(
         kwonly_defaults,
         _,
     ) = getfullargspec(unwrap(func))
-    function_name = name or func.__name__
+    if takes_context:
+        assert params[0] == 'context', ('First param of custom sandwich tag must be context '
+                                        'if `takes_context` is True')
 
     def compile_func(parser, token):
-        nonlocal params, varargs, varkw, defaults, kwonly
 
         open_sw_tag, *bits = token.split_contents()
         close_sw_tag = "end" + open_sw_tag
@@ -64,169 +45,77 @@ def make_sandwich_tag(
             defaults=defaults,
             kwonly=kwonly,
             kwonly_defaults=kwonly_defaults,
-            takes_context=False,
+            takes_context=takes_context,
             name=open_sw_tag,
         )
-        return SandwichTagNode(func, child_nodelist, template, token_args, token_kwargs, child_var_name)
-    compile_func.__name__ = "Compile"+function_name
+        return SandwichTagNode(func, child_nodelist, template, token_args, token_kwargs, child_var_name, takes_context)
+    compile_func.__name__ = function_name
     return compile_func
 
 
 def register_sandwich_tag(
-        func=None,
-        template: str=None,
-        name: str=None,
+        template: str,
         registry: Library=None,
+        name: str=None,
+        func=None,
         takes_context=False,
         child_var_name='sandwich_fixings'
 ):
-    template = get_template(template)
     def dec(func):
-        (
-            params,
-            varargs,
-            varkw,
-            defaults,
-            kwonly,
-            kwonly_defaults,
-            _,
-        ) = getfullargspec(unwrap(func))
-
-        function_name = name or func.__name__
 
         @wraps(func)
         def compile_func(parser, token):
-            nonlocal params, varargs, varkw, defaults, kwonly
+            return get_compile_func(
+                func=func,
+                template=template,
+                name=name,
+                takes_context=takes_context,
+                child_var_name=child_var_name,
+            )(parser, token)
 
-            open_sw_tag, *bits = token.split_contents()
-            close_sw_tag = "end" + open_sw_tag
-            child_nodelist = parser.parse(parse_until=(close_sw_tag,))
-            parser.delete_first_token()
-
-            token_args, token_kwargs = parse_bits(
-                parser=parser,
-                bits=bits,
-                params=params,
-                varargs=varargs,
-                varkw=varkw,
-                defaults=defaults,
-                kwonly=kwonly,
-                kwonly_defaults=kwonly_defaults,
-                takes_context=False,
-                name=open_sw_tag,
-            )
-            return SandwichTagNode(func, child_nodelist, template, token_args, token_kwargs, child_var_name)
-        registry.tag(function_name, compile_func)
+        registry.tag(compile_func.__name__, compile_func)
         return func
 
     if func is None:
-        # @register.simple_tag(...)
         return dec
     elif callable(func):
-        # @register.simple_tag
         return dec(func)
     else:
         raise ValueError("Invalid arguments provided to register_sandwich_tag")
 
 
-
-
-
 def add_sandwich_tag_dec(registry: Library):
     def sandwich_tag(
-            template,
-            func=None,
-            name: str = None,
-            takes_context=False,
-            child_var_name='sandwich_fixings'
+        template: str,
+        name: str=None,
+        func=None,
+        takes_context=False,
+        child_var_name='sandwich_fixings'
     ):
-        nonlocal registry
-        template = get_template(template)
-
-        def dec(func):
-            (
-                params,
-                varargs,
-                varkw,
-                defaults,
-                kwonly,
-                kwonly_defaults,
-                _,
-            ) = getfullargspec(unwrap(func))
-
-            function_name = name or func.__name__
-
-            @wraps(func)
-            def compile_func(parser, token):
-                nonlocal params, varargs, varkw, defaults, kwonly
-
-                open_sw_tag, *bits = token.split_contents()
-                close_sw_tag = "end" + open_sw_tag
-                child_nodelist = parser.parse(parse_until=(close_sw_tag,))
-                parser.delete_first_token()
-
-                token_args, token_kwargs = parse_bits(
-                    parser=parser,
-                    bits=bits,
-                    params=params,
-                    varargs=varargs,
-                    varkw=varkw,
-                    defaults=defaults,
-                    kwonly=kwonly,
-                    kwonly_defaults=kwonly_defaults,
-                    takes_context=False,
-                    name=open_sw_tag,
-                )
-                return SandwichTagNode(func, child_nodelist, template, token_args, token_kwargs, child_var_name)
-
-            registry.tag(function_name, compile_func)
-            return func
-
-        if func is None:
-            # @register.simple_tag(...)
-            return dec
-        elif callable(func):
-            # @register.simple_tag
-            return dec(func)
-        else:
-            raise ValueError("Invalid arguments provided to register_sandwich_tag")
+        return register_sandwich_tag(
+            template=template,
+            registry=registry,
+            name=name,
+            func=func,
+            takes_context=takes_context,
+            child_var_name=child_var_name,
+        )
     registry.sandwich_tag = sandwich_tag
     return registry
 
 
-
-
-
-
-
 # the wrapped function returns the context to render a sandwich tag
 class SandwichTagNode(TagHelperNode):
-    def __init__(self, func, child_nodelist, template, args, kwargs, child_var_name):
-        super().__init__(func, False, args, kwargs)
+    def __init__(self, func, child_nodelist, template, args, kwargs, child_var_name, takes_context):
+        super().__init__(func, takes_context, args, kwargs)
         self.child_nodelist = child_nodelist
         self.template = template
         self.child_var_name = child_var_name
 
     def render(self, context: Context):
-        # parent context is any kwargs passed to the sandwich tag, excluding `template` (see `do_sandwich`)
-        resolved_args, resolved_kwargs = self.get_resolved_arguments(context)
-        bread_context = self.func(*resolved_args, **resolved_kwargs)
-        bread_context[self.child_var_name] = self.child_nodelist.render(context)
-        return self.template.render(bread_context)
-
-
-if __name__ == "__main__":
-    from django.conf import settings
-    from django.apps import apps
-    from django.template.loader import get_template
-    from sandwich_tag.templatetags.demo_new import trial_tag
-    apps.apps_ready = True
-    trial_tag("Hello", 'some-url', 'some-arg', name="bob")
-
-    print(get_template('bread_demo.html').render({}))
-
-    # apps.apps_ready = True
-        # import os
-        # from django import setup
-        # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
-        # setup()
+        # Note: if `takes_context` is truthy, any changes made to context in `self.func` will only affect `child_nodelist`
+        with context.push():
+            resolved_args, resolved_kwargs = self.get_resolved_arguments(context)
+            bread_context = self.func(*resolved_args, **resolved_kwargs)
+            bread_context[self.child_var_name] = self.child_nodelist.render(context)
+            return self.template.render(bread_context)
